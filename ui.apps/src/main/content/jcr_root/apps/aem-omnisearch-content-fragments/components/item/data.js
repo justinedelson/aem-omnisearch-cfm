7c3420216513fe04bc2c552a59ad1c567408933d
Oscar Bolaños <bolanosc@adobe.com>
2016-08-30 13:58:15 -0700

    Added LastModified in the Card properties.

--- ui.apps/src/main/content/jcr_root/apps/aem-omnisearch-content-fragments/components/item/data.js
+++ ui.apps/src/main/content/jcr_root/apps/aem-omnisearch-content-fragments/components/item/data.js
@@ -1,10 +1,23 @@
+/*global org, com, java*/
 var CONTENT_FRAGMENT_EDITOR_VANITY = "/editor.html";
 
-/*global
-	org
-*/
 use(function() {
+    var resourceResolver, asset, lastModified, rtf, date, formattedRelativeTime;
+    
+    resourceResolver = resource.resourceResolver;
+    asset = resource.adaptTo(com.day.cq.dam.api.Asset);
+    if (asset) {
+        rtf = new com.day.cq.commons.date.RelativeTimeFormat('r', request.getResourceBundle(request.getLocale()));
+        lastModified = asset.getLastModified() || properties.get("jcr:created", "");
+        formattedRelativeTime = rtf.format(new Date(lastModified).getTime(), true);
+    } else {
+        lastModified = "";
+        formattedRelativeTime = "";
+    }
+
     return {
+        lastModified: lastModified,
+        formattedRelativeTime: formattedRelativeTime,
         navigationHref: request.getContextPath() + CONTENT_FRAGMENT_EDITOR_VANITY + org.apache.jackrabbit.util.Text.escapePath(resource.path),
         // default thumbnail path for content fragments
         thumbnailUrl: "/libs/dam/cfm/admin/content/static/thumbnail_fragment.png",
