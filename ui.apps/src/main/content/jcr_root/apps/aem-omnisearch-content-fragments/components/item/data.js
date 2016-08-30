bd62e6b3147804eaff547317fb680402615bc010
Oscar Bolaños <bolanosc@adobe.com>
2016-08-30 09:12:53 -0700

    Added correct navigationHref to the card.
    * Updated context title to be Fragment.

--- ui.apps/src/main/content/jcr_root/apps/aem-omnisearch-content-fragments/components/item/data.js
+++ ui.apps/src/main/content/jcr_root/apps/aem-omnisearch-content-fragments/components/item/data.js
@@ -1,6 +1,11 @@
+var CONTENT_FRAGMENT_EDITOR_VANITY = "/editor.html";
+
+/*global
+	org
+*/
 use(function() {
-	log.info(resource);
     return {
+        navigationHref: request.getContextPath() + CONTENT_FRAGMENT_EDITOR_VANITY + org.apache.jackrabbit.util.Text.escapePath(resource.path),
         // default thumbnail path for content fragments
         thumbnailUrl: "/libs/dam/cfm/admin/content/static/thumbnail_fragment.png",
         title : properties.get("jcr:content/jcr:title", resource.name),
