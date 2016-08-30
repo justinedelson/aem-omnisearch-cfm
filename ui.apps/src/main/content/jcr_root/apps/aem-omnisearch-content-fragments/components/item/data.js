a8820272a60f20f60dbc8f8502f672238e378a19
Oscar Bolaños <bolanosc@adobe.com>
2016-08-29 18:03:39 -0700

    Added image and context
    * Path is hardcoded as found here: https://git.corp.adobe.com/CQ/dam/blob/cb0559954785239c9ca1854162506dd79cfa76ba/content/jcr_root/libs/dam/gui/coral/components/admin/contentrenderer/card/assetomnisearch/assetViewer.jsp#L212
    * Added foundation-collection-item-title
    * Hardcoded DOCUMENT context because we have no other mimeType

--- ui.apps/src/main/content/jcr_root/apps/aem-omnisearch-content-fragments/components/item/data.js
+++ ui.apps/src/main/content/jcr_root/apps/aem-omnisearch-content-fragments/components/item/data.js
@@ -1,6 +1,8 @@
 use(function() {
 	log.info(resource);
     return {
+        // default thumbnail path for content fragments
+        thumbnailUrl: "/libs/dam/cfm/admin/content/static/thumbnail_fragment.png",
         title : properties.get("jcr:content/jcr:title", resource.name),
         description: properties.get("jcr:content/jcr:description", "")
     };
