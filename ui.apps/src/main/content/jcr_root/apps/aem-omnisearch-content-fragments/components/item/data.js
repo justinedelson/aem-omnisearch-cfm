bf0e46dc3d6de6e186796d8fa604fa82a6daa79e
Oscar Bolaños <bolanosc@adobe.com>
2016-08-31 11:07:04 -0700

    Added quick actions for edit and view properties

--- ui.apps/src/main/content/jcr_root/apps/aem-omnisearch-content-fragments/components/item/data.js
+++ ui.apps/src/main/content/jcr_root/apps/aem-omnisearch-content-fragments/components/item/data.js
@@ -1,11 +1,12 @@
 /*global org, com, java*/
 var CONTENT_FRAGMENT_EDITOR_VANITY = "/editor.html";
+var CONTENT_FRAGMENT_PROPERTIES_PATH = "/mnt/overlay/dam/cfm/admin/content/metadata-editor.html";
 
 use(function() {
-    var resourceResolver, asset, lastModified, rtf, date, formattedRelativeTime;
+    var asset, lastModified, rtf, formattedRelativeTime, escapedPath;
 
-    resourceResolver = resource.resourceResolver;
     asset = resource.adaptTo(com.day.cq.dam.api.Asset);
+    escapedPath = org.apache.jackrabbit.util.Text.escapePath(resource.path);
     if (asset) {
         rtf = new com.day.cq.commons.date.RelativeTimeFormat('r', request.getResourceBundle(request.getLocale()));
         lastModified = asset.getLastModified() || properties.get("jcr:created", "");
@@ -18,7 +19,8 @@ use(function() {
     return {
         lastModified: lastModified,
         formattedRelativeTime: formattedRelativeTime,
-        navigationHref: request.getContextPath() + CONTENT_FRAGMENT_EDITOR_VANITY + org.apache.jackrabbit.util.Text.escapePath(resource.path),
+        navigationHref: request.getContextPath() + CONTENT_FRAGMENT_EDITOR_VANITY + escapedPath,
+        propertiesHref: request.getContextPath() + CONTENT_FRAGMENT_PROPERTIES_PATH + escapedPath,
         // default thumbnail path for content fragments
         thumbnailUrl: "/libs/dam/cfm/admin/content/static/thumbnail_fragment.png",
         title : properties.get("jcr:content/jcr:title", resource.name),
